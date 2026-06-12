9f310189ef4afa85ad88921ea11cd8d3e1318082
Matteo Samsinger <wi24b176@technikum-wien.at>
2026-06-12 15:52:57 +0200

    Add RabbitMQ setup for energy user

--- energy-user/src/main/java/at/fhtechnikum/energyuser/EnergyUserApplication.java
+++ energy-user/src/main/java/at/fhtechnikum/energyuser/EnergyUserApplication.java
@@ -1,10 +1,17 @@
 package at.fhtechnikum.energyuser;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EnergyUserApplication {
+
+    @Bean
+    public Queue userQueue() {
+        return new Queue("user_mq", true);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EnergyUserApplication.class, args);
