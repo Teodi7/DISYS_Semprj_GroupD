11b87bafca8da14c00957063453d9447155a503a
Matteo Samsinger <wi24b176@technikum-wien.at>
2026-06-12 13:44:15 +0200

    Add RabbitMQ setup for energy producer

--- energy-producer/src/main/java/at/fhtechnikum/energyproducer/EnergyProducerApplication.java
+++ energy-producer/src/main/java/at/fhtechnikum/energyproducer/EnergyProducerApplication.java
@@ -1,13 +1,19 @@
 package at.fhtechnikum.energyproducer;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EnergyProducerApplication {
 
+    @Bean
+    public Queue producerQueue() {
+        return new Queue("producer_mq", true);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EnergyProducerApplication.class, args);
     }
-
 }
